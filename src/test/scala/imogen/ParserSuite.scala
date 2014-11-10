2db2b539ee590a59ad1130e2236d90c3b8f7e621
Sean McLaughlin <seanmcl@gmail.com>
2014-11-10 13:04:14 -0500

    More parser tests.

--- src/test/scala/imogen/ParserSuite.scala
+++ src/test/scala/imogen/ParserSuite.scala
@@ -6,6 +6,10 @@ class ParserSuite extends FunSuite {
   def ok(s: String, f: Formula) = assert(Parse.parseString(s) === f)
   def err(s: String) = intercept[ParseError](Parse.parseString(s))
 
+  test("Empty") {
+    err("")
+  }
+
   test("Top") {
     ok("T", Top)
   }
@@ -24,4 +28,23 @@ class ParserSuite extends FunSuite {
     notAtom("1")
     notAtom("1a")
   }
+
+  test("Conjunction Right Associative") {
+    ok("p & q & r", And(Atom("p"), And(Atom("q"), Atom("r"))))
+    ok("T & T & T & T", And(Top, And(Top, And(Top, Top))))
+    ok("(T & T) & T", And(And(Top, Top), Top))
+  }
+
+  test("Implication Right Associative") {
+    ok("p => q => r", Imp(Atom("p"), Imp(Atom("q"), Atom("r"))))
+    ok("T => T => T => T", Imp(Top, Imp(Top, Imp(Top, Top))))
+    ok("(T => T) => T", Imp(Imp(Top, Top), Top))
+  }
+
+  test("Implication Conjunction Precedence") {
+    ok("p & q => r", Imp(And(Atom("p"), Atom("q")), Atom("r")))
+    ok("p => q & r", Imp(Atom("p"), And(Atom("q"), Atom("r"))))
+    ok("T => T & T => T", Imp(Top, Imp(And(Top, Top), Top)))
+  }
+
 }
